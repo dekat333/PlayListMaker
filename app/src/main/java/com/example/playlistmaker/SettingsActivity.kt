b93b5836469de45d4d86609aa48df522ea70fd5a
dekat333 <138580032+dekat333@users.noreply.github.com>
2024-01-28 22:14:27 +0300

    Revert "Homework 9"
    This reverts commit 6f937330ad3c8f84ce136f9ed8e0bdd0e9696252.

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,17 +1,12 @@
 package com.example.playlistmaker
 
-import android.annotation.SuppressLint
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.FrameLayout
 import android.widget.ImageButton
-import android.widget.TextView
 
 class SettingsActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -23,35 +18,5 @@ class SettingsActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-
-        val button_share = findViewById<FrameLayout>(R.id.share)
-
-        button_share.setOnClickListener {
-            val shareIntent = Intent(Intent.ACTION_SENDTO)
-            shareIntent.data = Uri.parse("mailto:")
-            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.mail)))
-            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.web_practicum))
-            startActivity(shareIntent)
-        }
-
-        val button_support = findViewById<FrameLayout>(R.id.help)
-
-        button_support.setOnClickListener {
-            val supportIntent = Intent(Intent.ACTION_SENDTO)
-            supportIntent.data = Uri.parse("mailto:")
-            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.mail)))
-            supportIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.subject))
-            supportIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.message))
-            startActivity(supportIntent)
-        }
-
-        val button_user = findViewById<FrameLayout>(R.id.user_doc)
-
-        button_user.setOnClickListener {
-            val userIntent = Intent(Intent.ACTION_VIEW)
-            userIntent.data = Uri.parse(getString(R.string.offer))
-            startActivity(userIntent)
-        }
     }
-
 }
