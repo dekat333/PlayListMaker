c2a8904fd9a1e2e9de7f245bca81e0fcc262eec0
dekat333 <138580032+dekat333@users.noreply.github.com>
2024-02-04 03:07:57 +0300

    Homework 10

---
+++ app/src/main/java/com/example/playlistmaker/SearchViewHolder.kt
@@ -0,0 +1,42 @@
+package com.example.playlistmaker
+
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+class SearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val iconTrack: ImageView
+    private val trackName: TextView
+    private val authorTrack: TextView
+    private val timeTrack: TextView
+    private val point: ImageView
+    private val trackItem: ConstraintLayout
+    private val view: View
+
+    init {
+        iconTrack = itemView.findViewById(R.id.iconTrack)
+        trackName = itemView.findViewById(R.id.trackName)
+        authorTrack = itemView.findViewById(R.id.authorTrack)
+        timeTrack = itemView.findViewById(R.id.timeTrack)
+        point = itemView.findViewById(R.id.point)
+        trackItem = itemView.findViewById(R.id.trackItem)
+        view = itemView
+    }
+        fun bind(model: Track) {
+            trackName.text = model.trackName
+            authorTrack.text = model.artistName
+            timeTrack.text = model.trackTime
+            Glide
+                .with(trackItem)
+                .load(model.artworkUrl100)
+                .placeholder(R.drawable.placeholder)
+                .transform(RoundedCorners(10))
+                .into(iconTrack)
+        }
+    }
